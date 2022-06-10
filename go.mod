17a2bd6793212b4ff71feb17f8fb81bea192899d
riekyl <rieky@nusatalent.com>
2022-06-11 02:33:59 +0700

    add function greetings

---
+++ go.mod
@@ -0,0 +1,3 @@
+module github.com/riekylesmana/go-greetings
+
+go 1.18
---
+++ greetings.go
@@ -0,0 +1,8 @@
+package go_greetings
+
+func SayGoodMorning() string{
+	return "good morning"
+}
+func SayHello() string{
+	return "Hello"
+}
